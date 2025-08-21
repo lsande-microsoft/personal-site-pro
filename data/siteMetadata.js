9c1156219f07f2087ffd17b3298c9182dc9718de
Luis Sande <226566791+lsande-microsoft@users.noreply.github.com>
2025-08-21 12:04:40 -0400

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://LuisFelipeSande.personal.com',
+  siteUrl: 'https://personal-site-pro.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
